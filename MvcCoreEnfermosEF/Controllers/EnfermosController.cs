55f8e26256d65555aa6e57ec57eadf3009ced565
RazvanLucacii <rlucaci48@gmail.com>
2024-02-19 11:05:31 +0100

    Add project files.

---
+++ MvcCoreEnfermosEF/Controllers/EnfermosController.cs
@@ -0,0 +1,21 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using MvcCoreEnfermosEF.Models;
+using MvcCoreEnfermosEF.Repositories;
+
+namespace MvcCoreEnfermosEF.Controllers
+{
+    public class EnfermosController : Controller
+    {
+        RepositoryEnfermos repoEnf;
+
+        public EnfermosController(RepositoryEnfermos repo)
+        {
+            this.repoEnf = repo;
+        }
+        public IActionResult Index()
+        {
+            List<Enfermo> enfermos = this.repoEnf.GetEnfermos();
+            return View(enfermos);
+        }
+    }
+}
